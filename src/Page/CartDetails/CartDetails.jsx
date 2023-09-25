6aa36622f7eb54e22f53d5d67c24b90633a0e1f4
rabby-web <rabby.webdeveloper@gmail.com>
2023-09-25 12:20:51 +0600

    local storage added

--- src/Page/CartDetails/CartDetails.jsx
+++ src/Page/CartDetails/CartDetails.jsx
@@ -1,12 +1,34 @@
+import swal from "sweetalert";
+
 const CartDetails = ({ donation }) => {
-  const { card_img, price, description, category, style } = donation || {};
+  const { card_img, price, description, category, style, id } = donation || {};
   console.log(donation);
+  const handleAddToDonate = () => {
+    const addedDonate = [];
+    const donationsItem = JSON.parse(localStorage.getItem("donations"));
+
+    if (!donationsItem) {
+      addedDonate.push(donation);
+      localStorage.setItem("donations", JSON.stringify(addedDonate));
+      swal("Done!", "Your Donation Successfully!", "success");
+    } else {
+      const isExists = donationsItem.find((donation) => donation.id === id);
+      if (!isExists) {
+        addedDonate.push(...donationsItem, donation);
+        localStorage.setItem("donations", JSON.stringify(addedDonate));
+        swal("Done!", "Your Donation Successfully!", "success");
+      } else {
+        swal("Sorry!", "Already Donated!", "error");
+      }
+    }
+  };
   return (
     <div className="w-full md:w-2/3 mx-auto my-4">
       <div className="rounded-lg relative mx-3">
         <img className="w-full  h-auto rounded-lg" src={card_img} alt="" />
         <div className="absolute bottom-0 w-full bg-[#0B0B0B80] py-2 md:py-5 rounded-b-lg">
           <button
+            onClick={handleAddToDonate}
             className="py-2 px-4 m-3 rounded text-lg"
             style={{ backgroundColor: style?.t_b_color }}
           >
