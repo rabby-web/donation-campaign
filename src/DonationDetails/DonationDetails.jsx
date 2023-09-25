c024db528fe9ff216c6ff7148aac400d24bd5f3c
rabby-web <rabby.webdeveloper@gmail.com>
2023-09-25 13:31:20 +0600

    donaton details add to card and donation show cart

---
+++ src/DonationDetails/DonationDetails.jsx
@@ -0,0 +1,51 @@
+import { Link } from "react-router-dom";
+
+const DonationDetails = ({ donation }) => {
+  const { card_img, price, category, style, title } = donation || {};
+  return (
+    <div>
+      <div
+        style={{ backgroundColor: style?.card_bg }}
+        className="relative flex w-full max-w-[48rem] flex-row rounded-xl bg-white bg-clip-border text-gray-700 shadow-md"
+      >
+        <div className="relative m-0 w-2/5 shrink-0 overflow-hidden rounded-xl rounded-r-none bg-white bg-clip-border text-gray-700">
+          <img
+            src={card_img}
+            alt="image"
+            className="h-full w-full object-cover"
+          />
+        </div>
+        <div className="p-6">
+          <button
+            style={{ backgroundColor: style?.btn_bg, color: style?.t_b_color }}
+            className="mb-4 block font-sans text-base font-semibold leading-relaxed tracking-normal py-2 px-4 rounded antialiased"
+          >
+            {category}
+          </button>
+          <h4 className="mb-2 block font-sans text-2xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+            {title}
+          </h4>
+          <p
+            style={{ color: style?.t_b_color }}
+            className="mb-8 block font-sans text-base font-semibold leading-relaxed"
+          >
+            ${price}
+          </p>
+          <Link>
+            <a className="inline-block" href="#">
+              <button
+                style={{ backgroundColor: style?.t_b_color }}
+                className="flex select-none items-center gap-2 rounded-lg py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white transition-all hover:bg-pink-500/10 active:bg-pink-500/30 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                type="button"
+              >
+                View Details
+              </button>
+            </a>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DonationDetails;
--- src/Page/Donation/Donation.jsx
+++ src/Page/Donation/Donation.jsx
@@ -1,7 +1,50 @@
+import { useEffect, useState } from "react";
+import DonationDetails from "../../DonationDetails/DonationDetails";
+
 const Donation = () => {
+  const [donations, setDonations] = useState([]);
+  const [noFound, setNoFound] = useState(false);
+  const [isShow, setIsShow] = useState(false);
+  useEffect(() => {
+    const donationsItem = JSON.parse(localStorage.getItem("donations"));
+    if (donationsItem) {
+      setDonations(donationsItem);
+    } else {
+      setNoFound("No Data Found");
+    }
+  }, []);
   return (
-    <div>
-      <h1>Donation</h1>
+    <div className="container mx-auto">
+      {noFound && (
+        <p className="h-[80vh] flex justify-center items-center">{noFound}</p>
+      )}
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
+        {isShow
+          ? donations.map((donation) => (
+              <DonationDetails
+                key={donation.id}
+                donation={donation}
+              ></DonationDetails>
+            ))
+          : donations
+              .slice(0, 2)
+              .map((donation) => (
+                <DonationDetails
+                  key={donation.id}
+                  donation={donation}
+                ></DonationDetails>
+              ))}
+      </div>
+      <div>
+        {donations.length > 2 && (
+          <button
+            onClick={() => setIsShow(!isShow)}
+            className="px-5 bg-green-700 rounded py-2 my-4 text-white block mx-auto"
+          >
+            {isShow ? "See less" : "See more"}
+          </button>
+        )}
+      </div>
     </div>
   );
 };
